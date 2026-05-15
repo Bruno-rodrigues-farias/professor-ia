e181daca4e82d78e33b4ae8944f79f7378986efb
Bruno Rodrigues Farias <140970160+Bruno-rodrigues-farias@users.noreply.github.com>
2026-05-15 16:17:49 -0300

    fix render port

--- backend/server.js
+++ backend/server.js
@@ -532,8 +532,9 @@ app.get("/api/tasks/reviews", auth, (req, res) => {
   }
 });
 
+
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
